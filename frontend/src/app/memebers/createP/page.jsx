3e951a9536119901a0b10d11fb0ddbc7f5b9b678
Romeoteni188 <Romeoteni093@gmail.com>
2025-09-01 12:35:21 -0600

    fix(memebers): module create

--- frontend/src/app/memebers/createP/page.jsx
+++ frontend/src/app/memebers/createP/page.jsx
@@ -1,13 +1,176 @@
+import Link from "next/link"
+
 const CreateP = () => {
     return (
-        <div>
-            <div className="flex items-center justify-center w-full h-screen">
-                <h1 className="text-3xl font-bold">Nuevo Registro</h1>
-                <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-200">
-                    <form></form>
-                </div>
+        <div className="flex flex-col items-center justify-center w-full min-h-screen bg-gray-100 ">
+            <h1 className="text-2xl font-bold mb-4">Nuevo Registro</h1>
+            <div className="flex flex-wrap gap-2 col-span-full w-full ">
+                <Link
+                    href="/"
+                    className="btn btn-primary rounded-2xl sm:ml-10 ">
+                    Regresar
+                </Link>
+            </div>
+
+            <div className="w-full max-w-7xl bg-white p-3 rounded-lg shadow-lg mt-4">
+                <form className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                    <div className="flex flex-col">
+                        <label className="label">Foto</label>
+                        <input type="file" className="file-input" />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Identificación</label>
+                        <input
+                            type="text"
+                            placeholder="409011"
+                            className="input"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Nombre</label>
+                        <input
+                            type="text"
+                            placeholder="Ingrese su nombre"
+                            className="input"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Apellido</label>
+                        <input
+                            type="text"
+                            placeholder="Ingrese su apellido"
+                            className="input"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Correo</label>
+                        <input
+                            type="email"
+                            placeholder="Ingrese email"
+                            className="input"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Teléfono</label>
+                        <input
+                            type="text"
+                            placeholder="Número de teléfono"
+                            className="input"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Fecha de nacimiento</label>
+                        <input type="date" className="input" />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Sexo</label>
+                        <select className="input">
+                            <option value="">Seleccione</option>
+                            <option value="M">Masculino</option>
+                            <option value="F">Femenino</option>
+                        </select>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Dirección</label>
+                        <input
+                            type="text"
+                            placeholder="Ingrese su dirección"
+                            className="input"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Estado civil</label>
+                        <select className="input">
+                            <option value="">Seleccione</option>
+                            <option value="S">Soltero</option>
+                            <option value="C">Casado</option>
+                            <option value="D">Divorciado</option>
+                            <option value="V">Viudo</option>
+                        </select>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">
+                            Año de inicio en la iglesia
+                        </label>
+                        <input type="date" className="input" />
+                    </div>
+
+                    <div className="flex items-center space-x-2">
+                        <input
+                            type="checkbox"
+                            defaultChecked
+                            className="checkbox checkbox-neutral"
+                        />
+                        <label>Bautizado?</label>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Fecha de Bautismo</label>
+                        <input type="date" className="input" />
+                    </div>
+
+                    <div className="flex items-center space-x-2">
+                        <input
+                            type="checkbox"
+                            defaultChecked
+                            className="checkbox checkbox-neutral"
+                        />
+                        <label>Es servidor en nuestra iglesia?</label>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Procesos completados</label>
+                        <input
+                            type="text"
+                            placeholder="Ingrese procesos"
+                            className="input"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Grupo al que pertenece?</label>
+                        <select className="input">
+                            <option value="">Seleccione</option>
+                            <option value="S">Sí</option>
+                            <option value="N">No</option>
+                        </select>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">Área de servicio</label>
+                        <input
+                            type="text"
+                            placeholder="Ingrese área"
+                            className="input"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="label">
+                            Lo favorito en la iglesia
+                        </label>
+                        <input className="input" />
+                    </div>
+
+                    <div className="flex flex-wrap gap-2 col-span-full">
+                        <button className="btn btn-accent w-auto rounded-2xl">
+                            Guardar
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     )
 }
+
 export default CreateP
