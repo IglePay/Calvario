930a9113dd1e9ee8901f9cbf5fe356284464c442
Romeoteni188 <Romeoteni093@gmail.com>
2025-09-01 10:25:55 -0600

    feat(person): create module person

---
+++ frontend/src/app/memebers/create_p/page.jsx
@@ -0,0 +1,13 @@
+const CreateP = () => {
+    return (
+        <div>
+            <div className="flex items-center justify-center w-full h-screen">
+                <h1 className="text-3xl font-bold">Nuevo Registro</h1>
+                <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-200">
+                    <form></form>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default CreateP
