c76b7db5c8ef4eb615369e3705f0fc7b77823eca
Romeoteni188 <Romeoteni093@gmail.com>
2025-09-08 15:18:39 -0600

    feat(backend): login protect page

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    Body,
+    BadRequestException,
+    Res,
+} from '@nestjs/common';
+import type { Response } from 'express';
+
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,12 +14,26 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('login')
-    async login(@Body() body: { email: string; password: string }) {
+    async login(
+        @Body() body: { email: string; password: string },
+        @Res({ passthrough: true }) res: Response,
+    ) {
         const user = await this.authService.validateUser(
             body.email,
             body.password,
         );
-        return this.authService.login(user);
+        const token = await this.authService.login(user);
+
+        // Guardamos el token en cookie segura
+        res.cookie('jwt', token.access_token, {
+            httpOnly: true,
+            // secure: process.env.NODE_ENV === 'production', // solo https en prod
+            secure: false,
+            sameSite: 'lax',
+            maxAge: 1000 * 60 * 60 * 24, // 1 día
+        });
+
+        return { message: 'Login exitoso' };
     }
 
     @Post('register')
@@ -22,5 +44,11 @@ export class AuthController {
         if (existing) throw new BadRequestException('El usuario ya existe');
 
         return this.authService.register(body.name, body.email, body.password);
+    }
+
+    @Post('logout')
+    async logout(@Res({ passthrough: true }) res: Response) {
+        res.clearCookie('jwt');
+        return { message: 'Logout exitoso' };
     }
 }
---
+++ backend/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,5 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwAuthGuard extends AuthGuard('jwt') implements CanActivate {}
