b4c38623676a5aacca4a086c74f8c53a2636e322
Romeoteni188 <Romeoteni093@gmail.com>
2025-10-03 15:16:04 -0600

    feat(schedule): implemente permissionsguard

--- backend/src/schedule/schedule.controller.ts
+++ backend/src/schedule/schedule.controller.ts
@@ -13,13 +13,16 @@ import { ScheduleService } from './schedule.service';
 import { AuthGuard } from '../auth/auth.guard';
 import { CreateSchedulerDto } from './dto/create-schedule.dto';
 import { UpdateSchedulerDto } from './dto/update-schedule.dto';
+import { PermissionsGuard } from 'src/common/guards/permissions.guard';
+import { Permissions } from 'src/common/decorators/permissions.decorator';
 
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, PermissionsGuard)
 @Controller('service')
 export class ScheduleController {
     constructor(private readonly scheduleService: ScheduleService) {}
 
     @Get()
+    @Permissions('ver_horario')
     findAll(@Req() req: Request & { user: any }) {
         const tenantId = req.user.tenantId;
         return this.scheduleService.findAll(tenantId);
