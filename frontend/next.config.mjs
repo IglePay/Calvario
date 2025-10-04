5cf34086fe24a451dd7438d83fbda0ad673adde3
Romeoteni188 <Romeoteni093@gmail.com>
2025-10-03 21:00:13 -0600

    feat(pwa,robots): format code

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,17 @@
+import path from "path"
+import { fileURLToPath } from "url"
 import withPWA from "next-pwa"
+
+// Convierte import.meta.url en ruta absoluta
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     poweredByHeader: false,
     reactStrictMode: true,
     turbopack: {
-        root: ".",
+        root: path.resolve(__dirname),
     },
 }
 
@@ -14,6 +20,7 @@ const pwaConfig = {
     dest: "public",
     register: true,
     skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
 }
 
 export default withPWA(pwaConfig)(nextConfig)
